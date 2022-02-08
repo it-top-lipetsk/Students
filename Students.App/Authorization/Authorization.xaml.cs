4adda12a39981a4c123a5cb6b2027b5031a4e46a
Starinin Andrey <starininandrey@gmail.com>
2022-02-08 21:05:12 +0300

    Update authorization

--- Students.App/Authorization/Authorization.xaml.cs
+++ Students.App/Authorization/Authorization.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Students.App.Authorization
 {
@@ -7,6 +8,22 @@ namespace Students.App.Authorization
         public Authorization()
         {
             InitializeComponent();
+            Closing += ClosedWindow;
+        }
+
+        private void ClosedWindow(object sender, EventArgs e)
+        {
+            var res = MessageBox.Show("Хотите выйти?", "Closed", MessageBoxButton.YesNo);
+
+            if (res == MessageBoxResult.Yes)
+            {
+                //TODO Закрытие всего приложения
+            }
+        }
+
+        private void ButtonAuth_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
