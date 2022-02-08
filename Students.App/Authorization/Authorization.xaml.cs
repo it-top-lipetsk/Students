4e1dd0631bd8ecf3b28eb58df96cb6d9ab5991de
Starinin Andrey <starininandrey@gmail.com>
2022-02-08 20:24:16 +0300

    Add authorization

---
+++ Students.App/Authorization/Authorization.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Students.App.Authorization
+{
+    public partial class Authorization : Window
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+    }
+}
